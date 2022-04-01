921376459b517726a3d2b9eaf6bf532ed73f9ea0
anderson-justin <jleeand@outlook.com>
2022-04-01 14:44:16 -0400

    add code for camera access

--- img-filters/src/main.rs
+++ img-filters/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-  //  println!("Hello, world!");
+use anyhow::Result; // Automatically handle the error types
+use opencv::{
+    prelude::*,
+    videoio,
+    highgui
+}; 
+fn main() -> Result<()> {
+    highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    let mut frame = Mat::default();
+    loop {
+        cam.read(&mut frame)?;
+        highgui::imshow("window", &frame)?;
+        let key = highgui::wait_key(1)?;
+        if key == 113 { 
+            break;
+        }
+    }
+    Ok(())
 }
